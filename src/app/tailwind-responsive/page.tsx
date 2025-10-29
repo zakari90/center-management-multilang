87a50611247c87f47b3d4eab520d257f18cec6d0
zakari90 <zakariazinedine1@gmail.com>
2025-10-29 14:16:42 +0100

    pwa

--- src/app/tailwind-responsive/page.tsx
+++ src/app/tailwind-responsive/page.tsx
@@ -1,5 +1,8 @@
 import TailwindResponsiveExample from '@/components/examples/tailwind-responsive-example'
 
+
 export default function TailwindResponsivePage() {
+  console.log("TailwindResponsivePage");
+  
   return <TailwindResponsiveExample />
 }
