edf46151a2be15abaa3a3076a3b961ec6154f348
zakari90 <zakariazinedine1@gmail.com>
2025-10-29 10:11:11 +0100

    pwa 2

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getSession } from "@/lib/authentication";
-import { Building2 } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function AuthLayout({
@@ -21,9 +20,9 @@ export default async function AuthLayout({
         <div className="max-w-3xl mx-auto p-6">
           <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
-                <div className="flex items-center gap-2 self-center font-medium">
+                {/* <div className="flex items-center gap-2 self-center font-medium">
                   <Building2 className="size-12 text-blue-500 " />
-                </div>
+                </div> */}
   
                     {children}
       {/* <div className="text-muted-foreground text-center text-xs text-balance">
