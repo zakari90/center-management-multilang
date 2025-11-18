82feeaa2d6d9894d839d351dd5e784f6926f5275
zakari90 <zakariazinedine1@gmail.com>
2025-11-18 22:38:48 +0100

    schema

--- src/components/studentReceiptTable.tsx
+++ src/components/studentReceiptTable.tsx
@@ -289,7 +289,7 @@ export default function StudentReceiptTable() {
               {t("exportCSV")}
             </Button>
             <Button asChild>
-              <Link href="/receipts/create">
+              <Link href="/manager/receipts/create">
                 <Plus className="mr-2 h-4 w-4" />
                 {t("newPayment")}
               </Link>
