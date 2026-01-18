ce5464a2fb025dbe54bb9cb91576d00cbaa2960f
zakari90 <zakariazinedine1@gmail.com>
2026-01-18 20:58:49 +0100

    correction

--- src/components/quickActions.tsx
+++ src/components/quickActions.tsx
@@ -1,43 +1,47 @@
 'use client'
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Calendar, GraduationCap, Receipt, UserPlus } from 'lucide-react'
+import { Calendar, GraduationCap, Receipt, ReceiptText, UserPlus } from 'lucide-react'
 import { ModalLink } from '@/components/modal-link'
 import { useTranslations } from 'next-intl'
+import AddStudentDialog from './AddStudentDialog'
+import AddStudentPaymentDialog from './AddStudentPaymentDialog'
+import AddTeacherDialog from './AddTeacherDialog'
+import AddTeacherPaymentDialog from './AddTeacherPaymentDialog'
 
 export default function QuickActions() {
   const t = useTranslations('QuickActions')
 
-  const actions = [
-    {
-      title: t('actions.addStudent.title'),
-      description: t('actions.addStudent.description'),
-      icon: UserPlus,
-      href: '/manager/students/create',
-      color: 'text-blue-600 bg-blue-100'
-    },
-    {
-      title: t('actions.addTeacher.title'),
-      description: t('actions.addTeacher.description'),
-      icon: GraduationCap,
-      href: '/manager/teachers/create',
-      color: 'text-purple-600 bg-purple-100'
-    },
-    {
-      title: t('actions.newReceipt.title'),
-      description: t('actions.newReceipt.description'),
-      icon: Receipt,
-      href: '/manager/receipts/create',
-      color: 'text-orange-600 bg-orange-100'
-    },
-    {
-      title: t('actions.schedule.title'),
-      description: t('actions.schedule.description'),
-      icon: Calendar,
-      href: '/manager/schedule',
-      color: 'text-pink-600 bg-pink-100'
-    }
-  ]
+  // const actions = [
+  //   {
+  //     title: t('actions.addStudent.title'),
+  //     description: t('actions.addStudent.description'),
+  //     icon: UserPlus,
+  //     href: '/manager/students/create',
+  //     color: 'text-blue-600 bg-blue-100'
+  //   },
+  //   {
+  //     title: t('actions.addTeacher.title'),
+  //     description: t('actions.addTeacher.description'),
+  //     icon: GraduationCap,
+  //     href: '/manager/teachers/create',
+  //     color: 'text-purple-600 bg-purple-100'
+  //   },
+  //   {
+  //     title: t('actions.newReceipt.title'),
+  //     description: t('actions.newReceipt.description'),
+  //     icon: Receipt,
+  //     href: '/manager/receipts/create',
+  //     color: 'text-orange-600 bg-orange-100'
+  //   },
+  //   {
+  //     title: t('actions.schedule.title'),
+  //     description: t('actions.schedule.description'),
+  //     icon: Calendar,
+  //     href: '/manager/schedule',
+  //     color: 'text-pink-600 bg-pink-100'
+  //   }
+  // ]
 
   return (
     <Card className="col-span-4">
@@ -47,19 +51,38 @@ export default function QuickActions() {
       </CardHeader>
       <CardContent>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
-          {actions.map((action) => (
-            <ModalLink key={action.href} href={action.href}>
+            <div >
               <div className="flex flex-col items-center gap-2 p-4 rounded-lg border hover:bg-accent transition-colors cursor-pointer">
-                <div className={`h-12 w-12 rounded-full ${action.color} flex items-center justify-center`}>
-                  <action.icon className="h-6 w-6" />
+                <div className={`h-12 w-12 rounded-full flex items-center justify-center`}>
+                  <UserPlus className="h-6 w-6" />
                 </div>
-                <div className="text-center">
-                  <p className="text-sm font-medium">{action.title}</p>
-                  <p className="text-xs text-muted-foreground">{action.description}</p>
-                </div>
+                <AddTeacherDialog/>
               </div>
-            </ModalLink>
-          ))}
+            </div>
+            <div >
+              <div className="flex flex-col items-center gap-2 p-4 rounded-lg border hover:bg-accent transition-colors cursor-pointer">
+                <div className={`h-12 w-12 rounded-full flex items-center justify-center`}>
+                  <GraduationCap className="h-6 w-6" />
+                </div>
+                <AddStudentDialog/>
+              </div>
+            </div>
+            <div >
+              <div className="flex flex-col items-center gap-2 p-4 rounded-lg border hover:bg-accent transition-colors cursor-pointer">
+                <div className={`h-12 w-12 rounded-full flex items-center justify-center`}>
+                  +<ReceiptText className="h-6 w-6" />
+                </div>
+                <AddStudentPaymentDialog/>
+              </div>
+            </div>
+            <div >
+              <div className="flex flex-col items-center gap-2 p-4 rounded-lg border hover:bg-accent transition-colors cursor-pointer">
+                <div className={`h-12 w-12 rounded-full flex items-center justify-center`}>
+                - <ReceiptText className="h-6 w-6" />
+                </div>
+                <AddTeacherPaymentDialog/>
+              </div>
+            </div>
         </div>
       </CardContent>
     </Card>
