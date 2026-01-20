c564fc643526537e67d7f997aeec3ccc95b2e2f5
zakari90 <zakariazinedine1@gmail.com>
2026-01-20 18:30:41 +0100

    delete dynamic routes

--- src/components/studentReceiptTable.tsx
+++ src/components/studentReceiptTable.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useCallback } from 'react'
 import { useRouter } from 'next/navigation'
-import Link from 'next/link'
+import { Link } from '@/i18n/navigation'
 import { ModalLink } from '@/components/modal-link'
 import { receiptActions, studentActions } from '@/lib/dexie/dexieActions'
 import { useAuth } from '@/context/authContext'
