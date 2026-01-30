98e37353573e73bb553077ea6a89e240b9d42759
zakari90 <zakariazinedine1@gmail.com>
2026-01-30 21:47:27 +0100

    correction

--- src/app/[locale]/admin/receipts/page.tsx
+++ src/app/[locale]/admin/receipts/page.tsx
@@ -1,18 +1,11 @@
 import AdminReceiptsTable from "@/components/inUse/adminReceiptPresenation";
-import StudentsTable from "@/components/studentsPresentation";
-import TeachersTable from "@/components/teachersPresentation";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  console.log("[AdminReceiptsPage] Server render", {
-    timestamp: new Date().toISOString(),
-  });
   return (
     <div>
       <AdminReceiptsTable />
-      <StudentsTable />
-      <TeachersTable />
     </div>
   );
 }
