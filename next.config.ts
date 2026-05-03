91670903a5bc1bddb986b219065801ece212c1d5
zakari90 <zakariazinedine1@gmail.com>
2026-05-03 21:24:47 +0100

    fix: switch to webpack and configure serwist to resolve vercel build error

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import createNextIntlPlugin from "next-intl/plugin";
+import withSerwistInit from "@serwist/next";
 
 const withNextIntl = createNextIntlPlugin();
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/worker/index.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig = {
   output: "standalone" as const,
   reactStrictMode: true,
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  turbopack: {
-    // resolveAlias: {
-    //   canvas: "./empty-module.ts",
-    // },
-    root: __dirname,
-  },
   async headers() {
     return [
       {
@@ -42,4 +42,4 @@ const nextConfig = {
   },
 };
 
-export default withNextIntl(nextConfig as any);
+export default withSerwist(withNextIntl(nextConfig as any));
